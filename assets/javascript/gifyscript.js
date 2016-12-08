48fba93498efef2c4b2cf35d21a1794da062d40b
antoniojgage <antoniojgage@gmail.com>
2016-12-08 16:22:45 -0600

    Fixed animate function.

--- assets/javascript/gifyscript.js
+++ assets/javascript/gifyscript.js
@@ -14,29 +14,41 @@ $(document).ready(function() {
         $("#gifs-appear-here").empty();
     }
 
+    function animate() {
+        var animateLink = $(this).data("animate");
+        $(this).attr("src", animateLink);
+        console.log(animateLink);
+    }
+
     function queryData() {
         var selected = $(this).text();
         console.log("selected= " + selected);
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-            selected + "&api_key=dc6zaTOxFJmzC&limit=5";
+            selected + "&api_key=dc6zaTOxFJmzC&limit=10";
         $.ajax({
                 url: queryURL,
                 method: "GET"
             })
             .done(function(response) {
                 var results = response.data;
+                console.log(results);
                 for (var i = 0; i < results.length; i++) {
                     var gifDiv = $("<div class='item'>");
                     var rating = results[i].rating;
                     var p = $("<p>").text("Rating: " + rating);
-                    var personImage = $("<img>");
-                    personImage.attr("src", results[i].images.fixed_height.url);
+                    var personImage = $("<img class='gif'>");
+
+                    personImage.attr("src", results[i].images.original_still.url);
+                    personImage.attr("data-animate", results[i].images.original.url);
                     gifDiv.prepend(p);
                     gifDiv.prepend(personImage);
                     $("#gifs-appear-here").prepend(gifDiv);
                 }
             });
     };
+
+
+
     $("#add-option").on("click", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
@@ -49,4 +61,5 @@ $(document).ready(function() {
     });
     $(document).on("click", ".gifSearch", queryData);
     $(document).on("click", "#Clear_Result", emptyData);
+    $(document).on("click", ".gif", animate);
 }); //end of code
